e88e9576fbabac368dd0e86714b8d1496f5af7cf
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-08-30 17:35:52 +0200

    ADMINNNNNNN

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ dyv.classList.add('statusdyv');
 dyv.innerHTML = '<p>TEST OZNÁMENÍ...</p>';
 dyv.id = 'statusdyv';
 document.body.prepend(dyv);
+
+async function showStatus() {
+    const res = await fetch('/get');
+    const data = await res.json();
+    const message = data.message || 'Žádná nová oznámení.';
+    const statusDiv = document.getElementById('statusdyv');
+    statusDiv.innerHTML = `<p>${message}</p>`;
+    statusDiv.style.display = 'block';
+    /*setTimeout(() => {
+        statusDiv.style.display = 'none';
+    }, 5000);*/
+}
+
+setInterval(async () => await showStatus(), 5000);
